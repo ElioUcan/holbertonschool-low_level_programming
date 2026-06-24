c167ab1eaa892110c9579cbdda45d36a26abe4ee
Elio Ucan <eliozapata2006@gmail.com>
2026-06-24 08:22:47 -0600

    First try

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/** 
+ * main - prints the size of various types on the computer it is compiled and run on.
+ * 
+ * Return: 0 for success
+ * 
+*/
+int main(void)
+{
+printf("Size of a %s: %lu byte(s)\n", "char", sizeof(char));
+return (0);
+}
