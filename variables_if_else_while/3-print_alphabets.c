0bef0a70ccf34d52e5c746a01f64456168df818e
Elio Ucan <eliozapata2006@gmail.com>
2026-06-24 13:34:13 -0600

    Who knows try

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: 0 if correct
+ */
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+for (i = 65; i <= 90; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
