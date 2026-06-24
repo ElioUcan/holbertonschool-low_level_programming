2679c6174e0cadd491e81bbd968357cfe424806d
Elio Ucan <eliozapata2006@gmail.com>
2026-06-24 14:47:35 -0600

    Who knows try

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0 if correct
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+if (i != 57)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(i);
+}
+}
+return (0);
+}
