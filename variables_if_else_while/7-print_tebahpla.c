4c7789768fb6aa613c5ef23ec7b26eedc079bd2f
Elio Ucan <eliozapata2006@gmail.com>
2026-06-24 14:35:44 -0600

    Who knows try

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: 0 if correct
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 97 ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
