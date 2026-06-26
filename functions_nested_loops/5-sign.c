a24c4f09e90d708f086e190706972d1da87870ff
Elio Ucan <eliozapata2006@gmail.com>
2026-06-26 12:06:05 -0600

    Second commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * @n: int parameter
  *
  * Return: 1 and prints + if n is greather than zero
- * Return: 0  and prints 0 if n is zero
- * Return: -1 and prints - if n is less than zero
+ * 0  and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
