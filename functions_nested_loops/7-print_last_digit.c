4c7778ce75bc2924ff4aa836db9e60ce340998b7
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 15:58:41 -0600

    Second commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 /**
  * print_last_digit - prints the last number
  * @v: number
- * 
+ *
  * Return: Returns the value of the last digit
  */
 int print_last_digit(int v)
 {
 if (v < 0)
 {
-v *= -1;    
+v *= -1;  
 }
 return (v % 10);
 }
