bd0c8802f53e5df28ff7fe8986a1c2cea6d61815
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 15:20:56 -0600

    Second commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @v: int to be used
+ * Return: abs int if correct else 0
+ */
+
+int _abs(int v)
+{
+if (v < 0)
+{
+return (v*-1);
+}
+return (0);
+}
