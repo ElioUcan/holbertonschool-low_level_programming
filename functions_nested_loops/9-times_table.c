fadee6985f11ed6daf418a4e1672cdfa3051eaef
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 18:15:56 -0600

    Second commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,18 +15,18 @@ for (i = 0; i <= 10; i++)
 {
 for (j = 0; j <= 10; j++)
 {
+r = j * i;
+if (r >=10)
+{
+_putchar('0' + r / 10);
+}
+_putchar('0' + r % 10);
+}
 if (j != 9)
 {
-r = j * i;
-_putchar('0' + r / 10);
 _putchar(',');
 _putchar(' ');
 }
-else
-{
-_putchar('0' + r / 10);
-}
 }
 _putchar('\n');
-}
 }
