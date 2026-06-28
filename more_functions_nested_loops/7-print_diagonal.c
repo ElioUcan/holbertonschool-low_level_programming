2fce01ddf295f2a2cf25ff20acc57af88c5a13fd
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 23:23:45 -0600

    Second commit

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the termina
+ * @b: int
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar("'\'");
+}
+_putchar('\n');
+}
+}
