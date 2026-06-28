880a6efd8fa093d193b1064962d99fb7de25f59d
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 18:44:15 -0600

    Second commit

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints the number till 98
+ *
+ * Return: nothing
+ */
+
+
+void print_to_98(int n)
+{
+while (n <= 98)
+{
+printf(n);
+n++;
+}
+}
