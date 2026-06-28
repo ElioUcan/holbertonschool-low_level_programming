351a678893a0640f0eeabba55ed3b084995b7a3a
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 23:50:07 -0600

    Second commit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Fizzbuzz classic
+ *
+ * Return: 0 if correct
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 100; i++)
+{
+if (i % 5 == 0 && i % 3 == 0)
+{
+printf('FizzBuzz');
+}
+else if (i % 5 == 0)
+{
+printf('Buzz');
+}
+else if (i % 3 == 0)
+{
+printf('Fizz');
+}
+else
+{
+printf('%d', i);
+}
+}
+}
