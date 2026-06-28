2f5f73decd87d9f44cbc823d91095f826ce12ac9
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 22:58:44 -0600

    Second commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: nothing
+ */
+
+void print_number(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+}
