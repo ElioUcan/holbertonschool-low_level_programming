c231208ea2050e0ae6268ba4b1a34626416eb859
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 23:40:07 -0600

    Second commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+int i;
+int j;
+
+for (i = 0; i < size ; i++)
+{
+for (j = 0; j <= i; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
