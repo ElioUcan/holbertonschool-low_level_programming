8b67912fb30e990a24d29f9fa06bee8ab9b906a2
Elio Ucan <eliozapata2006@gmail.com>
2026-06-27 18:53:36 -0600

    Second commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,21 @@
 
 void print_to_98(int n)
 {
+if (n > 98)
+{
+while (n >= 98)
+{
+if (n == 98)
+{
+printf("%i\n", n);
+break; 
+}
+printf("%i, ", n);
+n--;
+}
+}
+else
+{
 while (n <= 98)
 {
 if (n == 98)
@@ -18,5 +33,6 @@ break;
 }
 printf("%i, ", n);
 n++;
+}
 }
 }
