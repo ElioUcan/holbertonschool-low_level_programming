d21c49f93f008ac1f7c6a405cf554029ae49cc95
Elio Ucan <eliozapata2006@gmail.com>
2026-06-29 11:59:02 -0600

    First commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes an int pointer and update the value to 98
+ * @n: pointer
+ * 
+ * Return: nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
