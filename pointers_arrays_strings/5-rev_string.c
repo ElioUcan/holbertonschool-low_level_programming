960c7e28ad7c01f411d96ae40b5835bcecca69e0
Elio Ucan <eliozapata2006@gmail.com>
2026-07-01 08:23:25 -0600

    commit

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: char
+ * Return: nothing
+ */
+
+ void rev_string(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+for (; i >= 0 ; i--)
+{
+if (s[i] != '\n' && s[i] != '\0')
+{
+write(1, &s[i], 1);
+}
+}
+write(1, "\n", 1);
+}
