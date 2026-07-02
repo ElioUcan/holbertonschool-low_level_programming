75686cdaa4a43eead7e1316141ea701ddcd7a0b5
Elio Ucan <eliozapata2006@gmail.com>
2026-07-02 16:21:56 -0600

    commit

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,25 @@
 void puts_half(char *str)
 {
 int i = 0;
-int j = 0;
+int n;
 while (str[i] != '\0')
 {
 i++;
 }
+if (i % 2 == 0)
+{
+n = (i / 2);
+}
+else
+{
+n = ((i + 1) / 2);
+}
 
-while (str[i] != '\0')
+while (str[n] != '\0')
 {
-if (i == j)
-{
-break;
+write(1, &str[n], 1);
+n++;
 }
-write(1, &str[i], 1);    
-j++;
-i--;
-}
+
 write(1, "\n", 1);
 }
