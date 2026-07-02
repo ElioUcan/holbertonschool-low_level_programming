9c0c5f59b6b9b0b222e9bcc47a4699de874fa08a
Elio Ucan <eliozapata2006@gmail.com>
2026-07-02 10:27:01 -0600

    commit

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -23,7 +23,7 @@ for (; i >= 0; i--)
 {
 if (i % 2 == 0)
 {
-if (str[i] != '\0' && str[i] != '\n')
+if (str[i] != '\n')
 {
 write(1, &str[j], 1);
 }
@@ -31,4 +31,10 @@ write(1, &str[j], 1);
 j++;
 }
 write(1, "\n", 1);
+}
+
+int main(void){
+
+    puts2("Holberton!");
+    return (0);
 }
