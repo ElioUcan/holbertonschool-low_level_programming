b00ef0888e5ecec2aab91a95c618aa88f8e97bb8
Elio Ucan <eliozapata2006@gmail.com>
2026-07-02 19:42:18 -0600

    commit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strncat - concatenates two strings
  * @dest: destination
@@ -10,17 +11,40 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
+int count = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-while (n >= 0)
+while (src[j] != '\0' && count <= n)
 {
 dest[i] = src[j];
 j++;
+count++;
 }
+
+
 i++;
 dest[i] = '\0';
 
 return (dest);
+}
+
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
