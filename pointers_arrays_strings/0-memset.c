8723f5433af65fa99d38f55d9d35d0c86570352f
Elio Ucan <eliozapata2006@gmail.com>
2026-07-03 16:54:46 -0600

    commit

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - fills memory
+ * @s: pointer to fill
+ * @b: constant byte b
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+while (n >= 0)
+{
+s[i] = b;
+n--;
+i++;
+}
+
+return (s);
+}
