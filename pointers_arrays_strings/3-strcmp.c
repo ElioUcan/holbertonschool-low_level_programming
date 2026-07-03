0fe4bd6fc15fbdc9c0991e0e57b6906acee37159
Elio Ucan <eliozapata2006@gmail.com>
2026-07-02 20:44:39 -0600

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,10 @@ int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 int j = 0;
-while (s1[i] != '\0')
+while (s1[i] == s2[i] && s2[i] != '\0')
 {
 i++;
 }
-
-while (s2[j] != '\0')
-{
-j++;
-}
-i -= j;
-
-return (i);
+j = s1[i] - s2[i];
+return (j);
 }
