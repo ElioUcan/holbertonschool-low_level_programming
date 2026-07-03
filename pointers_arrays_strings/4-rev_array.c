ec5c94578cea0a029980410d7afff69d0c0ea269
Elio Ucan <eliozapata2006@gmail.com>
2026-07-02 20:50:04 -0600

    commit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: int value
+ * @n: int value
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+int i = 0;
+for (n; n >= 0; n--)
+{
+a[i] = a[n];
+i++;
+}
+}
