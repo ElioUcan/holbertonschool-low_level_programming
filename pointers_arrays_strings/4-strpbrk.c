f81a4ee0e479d40171c97d593aa879eaff200b64
Elio Ucan <eliozapata2006@gmail.com>
2026-07-04 16:09:26 -0600

    commit

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: char
+ * @accept: accept list
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i = 0;
+int j;
+
+if (s == NULL)
+{
+return (NULL);
+}
+
+while (s[i] != '\0')
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (s[i]);
+}
+}
+return (NULL);
+}
+}
