e19804aa509b525dd1ed5097756c9362422bbced
Elio Ucan <eliozapata2006@gmail.com>
2026-07-06 13:04:55 -0600

    commit

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *  which contains a copy of the string given as a parameter.
+ * @str: string
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+char *copy;
+int size = 0;
+int i;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+while (str[size] != '\0')
+{
+size++;
+}
+
+copy = malloc((size + 1) * sizeof(str));
+
+if (copy == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i <= size; i++)
+{
+copy[i] = str[i];
+}
+
+return (copy);
+}
