f4f516a048d6a3b77e9cfd5c35a55ff98ad542d4
Elio Ucan <eliozapata2006@gmail.com>
2026-07-06 13:34:51 -0600

    commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,45 +7,35 @@
  * @s2: string 1
  * Return: char
  */
-
-
 char *str_concat(char *s1, char *s2)
 {
 int size = 0;
 int i = 0;
 char *concatenate;
-
 if (s1 == NULL)
 {
 s1 = "";
 }
-
 if (s2 == NULL)
 {
 s2 = "";
 }
-
 while (s1[i] != '\0')
 {
 size++;
 i++;
 }
-
 i = 0;
-
 while (s2[i] != '\0')
 {
 size++;
 i++;
 }
-
 concatenate = malloc((size + 1) * sizeof(char));
-
 if (concatenate == NULL)
 {
 return (NULL);
 }
-
 for (i = 0; s1[i] != '\0'; i++)
 {
 concatenate[i] = s1[i];
@@ -55,6 +45,5 @@ for (i; s2[i] != '\0'; i++)
 {
 concatenate[i] = s2[i];
 }
-
 return (concatenate);
 }
