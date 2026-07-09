373f2fa23779c33a7204f05f7285cf7a0cdb0628
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 17:42:38 -0600

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,13 +10,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-
+int i;
+int j;
 d = malloc(sizeof(dog_t));
 if (d == NULL)
+{
+return (NULL);
+}
+while (name[i] != '\0')
+{
+i++;
+}
+while (owner[j] != '\0')
+{
+j++;
+}
+d->name = malloc((i + 1) * sizeof(char));
+if (d->name == NULL)
 {
 free(d);
 return (NULL);
 }
+
+d->owner = malloc((j + 1) * sizeof(char));
+if (d->owner == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+
 d->name = name;
 d->age = age;
 d->owner = owner;
