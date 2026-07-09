728add2cd8aad5319f38e5ae9dda683acb43cfb1
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 17:14:02 -0600

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef struct dog {
+/**
+ * struct dog - dog.
+ * @name: name of the dog.
+ * @owner: name of the owner.
+ * @age: age of the dog.
+ */
+typedef struct dog
+{
 char *name;
 char *owner;
 float age;
