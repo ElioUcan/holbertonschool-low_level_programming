1765c02478d7fed92ac3e771a7d95af536e8bbc3
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 17:47:49 -0600

    commit

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
 /**
- * 
+ * free_dog - frees dogs
+ * @d: dog struct
+ * Return: nothing
  */
-void free_dog(dog_t *d);
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
