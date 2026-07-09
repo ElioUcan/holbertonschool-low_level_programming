be8a71024abae1a92b08d4c18c089b34f2e277a3
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 16:36:52 -0600

    commit

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -6,4 +6,26 @@
  * @max: int
  * Return: int pointer
  */
-int *array_range(int min, int max);
+int *array_range(int min, int max)
+{
+int i;
+char *p;
+
+if (min > max)
+{
+return (NULL);
+}
+
+p = malloc(max * (sizeof(int)));
+if (p == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; min < max; i++)
+{
+p[i] = (min + i);
+}
+
+return (p);
+}
