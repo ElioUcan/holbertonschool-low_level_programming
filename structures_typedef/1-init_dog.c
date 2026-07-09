a354d3ae7f24ce0ad90f73dcce0e8d40fb4993a6
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 17:23:39 -0600

    commit

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * init_dog - initialize dog struct
  * @d: struct
@@ -9,6 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d == NULL)
+{
+puts("Ok");
+}
 d->name = name;
 d->age = age;
 d->owner = owner;
