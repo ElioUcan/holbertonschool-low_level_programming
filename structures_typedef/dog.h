1f21280296c5ee1e5fc06504e81552753aec6f92
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 17:20:39 -0600

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,9 @@ char *owner;
 float age;
 } Dog;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
