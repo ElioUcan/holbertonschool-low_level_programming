f41c3be7a6ba8655af0bd361a2d754d9704b1d98
Elio Ucan <eliozapata2006@gmail.com>
2026-07-09 17:36:17 -0600

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,24 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
- * 
+ * new_dog - creates a new dog
+ * @name: char
+ * @age: float
+ * @owner: owner
+ * Return: struct pointer
  */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+{
+return (NULL);
+}
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+return (d);
+}
