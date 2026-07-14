add73cc8456c816ae7dbc504d4ca5d54dcac5cfc
Elio Ucan <eliozapata2006@gmail.com>
2026-07-13 18:12:29 -0600

    commit

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * get_op_func - gets the functions
+ * @s: function
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int i;
+
+for (i = 0; ops[i].op[0] != NULL; i++)
+{
+if (ops[i].op[0] == s[0])
+{
+return (ops[i].f);
+}
+}
+return (NULL);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * main - invoques all the functions
+ * @args: the number of arguments
+ * @expr: the expression
+ * Return: 0 if correct.
+ */
+
+int main(int args, char *expr[])
+{
+int num1;
+int num2;
+int (*f)(int, int);
+
+if (args != 4)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (98);
+}
+num1 = atoi(expr[1]);
+num2 = atoi(expr[3]);
+
+f = get_op_func(expr[2]);
+
+if (f == NULL)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (99);
+}
+if ((*expr[2] == '/' || *expr[2] == '%') && num2 == 0)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (100);
+}
+
+}
