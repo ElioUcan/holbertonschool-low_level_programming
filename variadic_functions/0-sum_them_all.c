9016edcbdbee2812f91afd1ca97d83d33923d029
Elio Ucan <eliozapata2006@gmail.com>
2026-07-14 18:50:34 -0600

    commit

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sum all the number of argumenrs
+ * @n: int
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list numb;
+int total = 0;
+int i = 0;
+if (n == 0)
+{
+return (0);
+}
+va_start(numb, n);
+
+while (i <= n)
+{
+total += va_arg(numb, int);
+i++;
+}
+va_end(numb);
+return (total);
+}
