49cca002a26558f1c3cade9c9820f496329c48d9
Elio Ucan <eliozapata2006@gmail.com>
2026-07-16 18:11:15 -0600

    commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * program_name - prints the name of the program
+ * @argc: int
+ * @argv: array of strings
+ * Return: nothing
+ */
+
+void program_name(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+}
