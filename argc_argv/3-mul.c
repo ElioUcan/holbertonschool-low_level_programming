5b12428b2a42e7049fe8023059ba398927e1a137
Elio Ucan <eliozapata2006@gmail.com>
2026-07-16 18:50:24 -0600

    commit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -3,20 +3,25 @@
  * main - multiplies two numbers
  * @argc: int
  * @argv: array of strings
- * Return: 0 if success
+ * Return: 0 if success else 1
  */
 
 int main(int argc, char *argv[])
 {
 int i;
 int sum = 0;
-if (argc > 1)
+if (argc > 1 && argc > 3)
 {
 for (i = 0; i < argc; i++)
 {
 sum += atoi(argv[i]);
 }
-}
 printf("%i\n", sum);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
 return (0);
 }
