51afea96f5ebd82a87c04388bf21e841dc6cdb4d
Elio Ucan <eliozapata2006@gmail.com>
2026-07-16 18:16:40 -0600

    commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints the arguments passed
+ * @argc: int
+ * @argv: array of strings
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 1; i < argc; i++)
+{
+printf("%i\n", i);
+}
+}
