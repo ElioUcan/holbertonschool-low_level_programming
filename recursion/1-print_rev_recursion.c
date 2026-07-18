069033366ae2f80eafa43d94a528737a0f580da8
Elio Ucan <eliozapata2006@gmail.com>
2026-07-17 19:01:50 -0600

    commit

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,11 +7,10 @@
 
 void _print_rev_recursion(char *s)
 {
-if (s == NULL)
+if (*s == '\0')
 {
-_putchar('\n');
 return;
 }
+_print_rev_recursion(s + 1);
 _putchar(*s);
-_print_rev_recursion(s - 1);
 }
