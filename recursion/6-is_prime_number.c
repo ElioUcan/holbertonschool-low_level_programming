d8f7109a662e6c3df24457610ffb30196dec425b
Elio Ucan <eliozapata2006@gmail.com>
2026-07-22 09:53:45 -0600

    commit

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,43 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _puts_recursion - prints a string
- * @s: string
- * Return: nothing
+ * remember - 1 if primme otherwise 0
+ * @n: int
+ * @try: int
+ * Return: 1 if prime otherwise 0
  */
+
+int remember(int n, int try)
+{
+if (try > n)
+{
+return (0);
+}
+
+if ((try != 1 && try != n) && (n % try == 0))
+{
+return (0);
+}
+if (try == n)
+{
+return (1);
+}
+
+return (remember(n, try + 1));
+}
+
+/**
+ * is_prime_number - 1 if primme otherwise 0
+ * @n: int
+ * Return: 1 if prime otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+{
+return (0);
+}
+return (remember(n, 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -9,7 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int guess_square(int n, int try);
-
-
+int is_prime_number(int n);
+int remember(int n, int try);
 
 #endif
