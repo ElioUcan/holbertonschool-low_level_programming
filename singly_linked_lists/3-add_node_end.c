d237117da82007b8326165eb5b7b7638012bda1d
Elio Ucan <eliozapata2006@gmail.com>
2026-07-21 18:22:40 -0600

    commit

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,11 +8,13 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node;
+list_t *temp;
 int i = 0;
-if (head == NULL)
+if (*head == NULL)
 {
 return (NULL);
 }
+temp = *head;
 if (str != NULL)
 {
 while (str[i] != '\0')
@@ -20,19 +22,25 @@ while (str[i] != '\0')
 i++;
 }
 }
+
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 {
 return (NULL);
 }
+
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
-free(new_node->str);
+free(new_node);
 return (NULL);
 }
 new_node->len = i;
-new_node->next = *head;
-*head = new_node;
+
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+temp->next = new_node;
 return (new_node);
 }
