63de88cadd5a511ff6c2d0c5196662dda42a6765
Elio Ucan <eliozapata2006@gmail.com>
2026-07-27 17:31:37 -0600

    commit

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - gets a n node
+ * @head: node
+ * @index: int
+ * Return: node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+while (head != NULL)
+{
+if (i == index)
+{
+return (head);
+}
+head = head->next;
+}
+return (NULL);
+}
