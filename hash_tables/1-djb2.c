cb7871c10512ecc97a2016c6ba92ed9bdb709b25
Elio Ucan <eliozapata2006@gmail.com>
2026-07-28 17:09:04 -0600

    commit

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implements djb2 algorithm 
+ * hash_djb2 - implements djb2 algorithm
  * @str: uchar
  * Return: uli
  */
@@ -12,7 +12,7 @@ int c;
 hash = 5381;
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 }
 return (hash);
 }
