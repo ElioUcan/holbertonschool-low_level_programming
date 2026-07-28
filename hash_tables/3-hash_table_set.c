104f068a99b0bb9dcc9fd068965c4b78fbc1bba9
Elio Ucan <eliozapata2006@gmail.com>
2026-07-28 17:48:20 -0600

    commit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,26 +9,26 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int idx;
-int i;
-hash_node_t *temp;
+unsigned long int idx = key_index((const unsigned char *)key, ht->size);
+hash_node_t *temp = ht->array[idx];
+hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
 if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 {
 return (0);
 }
-idx = key_index(key, ht->size);
-temp = ht->array[idx];
+
 while (temp != NULL)
 {
 if (strcmp(temp->key, key) == 0)
 {
+free(new_node);
 free(temp->value);
 temp->value = strdup(value);
 return (1);
 }
 temp = temp->next;
 }
-hash_node_t *new_node = malloc(sizeof(hash_node_t));
 if (new_node == NULL)
 {
 return (0);
