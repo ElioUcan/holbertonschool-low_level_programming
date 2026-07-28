be69a32ef044e36fd00f1c0d1186d9257df5d2af
Elio Ucan <eliozapata2006@gmail.com>
2026-07-28 17:44:29 -0600

    commit

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: char
+ * @value: char
+ * Return: 1 good, 0 bad
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+unsigned long int idx;
+int i;
+hash_node_t *temp;
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+{
+return (0);
+}
+idx = key_index(key, ht->size);
+temp = ht->array[idx];
+while (temp != NULL)
+{
+if (strcmp(temp->key, key) == 0)
+{
+free(temp->value);
+temp->value = strdup(value);
+return (1);
+}
+temp = temp->next;
+}
+hash_node_t *new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+{
+return (0);
+}
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = ht->array[idx];
+ht->array[idx] = new_node;
+return (1);
+}
