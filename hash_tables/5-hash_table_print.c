66f59bbadff913eede5061d2b81cbfe9ab6566ff
Elio Ucan <eliozapata2006@gmail.com>
2026-07-28 18:09:39 -0600

    commit

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,17 +7,29 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+unsigned long int i;
+int f = 0;
 hash_node_t *temp;
 
 if (ht == NULL)
 {
 return;
 }
-temp = ht->array;
-
+for (i = 0; i < ht->size; i++)
+{
+temp = ht->array[i];
 while (temp)
 {
-printf("%s",temp->value);
+printf('{');
+if (f == 1)
+{
+printf(", ");
+}
+printf("'%s': '%s'", temp->key, temp->value);
+f == 1;
+printf('}\n');
 temp = temp->next;
 }
+}
+
 }
