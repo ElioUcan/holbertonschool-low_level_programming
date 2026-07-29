64450fddb3ae5d08e5e493679e249b1f1582d046
Elio Ucan <eliozapata2006@gmail.com>
2026-07-28 18:13:35 -0600

    commit

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -15,21 +15,20 @@ if (ht == NULL)
 {
 return;
 }
+printf("{");
 for (i = 0; i < ht->size; i++)
 {
 temp = ht->array[i];
 while (temp)
 {
-printf("{");
 if (f == 1)
 {
 printf(", ");
 }
 printf("'%s': '%s'", temp->key, temp->value);
-f == 1;
-printf("}\n");
+f = 1;
 temp = temp->next;
 }
 }
-
+printf("}\n");
 }
