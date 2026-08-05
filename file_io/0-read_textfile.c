72708821d1854a07d0c5c3987f5a0b5dfd29f218
Elio Ucan <eliozapata2006@gmail.com>
2026-08-04 18:58:33 -0600

    commit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,31 +9,45 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd;
-char buffer[letters];
+char *buffer;
 ssize_t bytesRead;
+ssize_t bytesWrite;
+
 
 
 if (filename == NULL)
-{
 return (0);
-}
+
 
 fd = open(filename, O_RDONLY);
 
 if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(letters));
+if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
 
-bytesRead = read(fd, buffer, sizeof(buffer));
+bytesRead = read(fd, buffer, letters);
 
 if (bytesRead == -1)
 {
+free(buffer);
 close(fd);
 return (0);
 }
 
+bytesWrite = write(STDOUT_FILENO, buffer, bytesRead);
+if (bytesWrite == -1 || bytesWrite != bytesRead)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
 close(fd);
 return (bytesRead);
 }
