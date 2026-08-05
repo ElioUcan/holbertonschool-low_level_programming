5a413ece0e4ca1d9827ee0d9a4d1b4e50fd8e3f9
Elio Ucan <eliozapata2006@gmail.com>
2026-08-04 19:01:10 -0600

    commit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,18 +13,15 @@ char *buffer;
 ssize_t bytesRead;
 ssize_t bytesWrite;
 
-
-
 if (filename == NULL)
 return (0);
-
 
 fd = open(filename, O_RDONLY);
 
 if (fd == -1)
 return (0);
 
-buffer = malloc(sizeof(letters));
+buffer = malloc(sizeof(char) * (letters + 1));
 if (buffer == NULL)
 {
 close(fd);
